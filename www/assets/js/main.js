308c0fca097d8892928cfafa6cc4c43e409b1d6c
Roman Mukhin <ramses_ru@hotmail.com>
2016-01-10 16:18:18 +0100

    Добавлены модальный диалог и инфо сообщения в HTML для поддержки покупки билетов через paypal

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,1 +1,28 @@
+function showTicketsModal() {
+	$('#TicketsModal').modal('show');
+}
+
+$(document).ready(function() {
+	var hStr = location.hash;
+
+	if (hStr.indexOf('tickets') >= 0) {
+		showTicketsModal();
+	}
+	
+	var qStr = window.location.search.substring(1)  
+  if (qStr.indexOf('paypal=success') >= 0) {
+    $("#ticket-success").removeClass("hidden");
+  }
+  else if (qStr.indexOf('paypal=cancel') >= 0) {
+    $("#ticket-cancel").removeClass("hidden");        	
+  }});
+
+$('a[href="#ticktes"]').click(function() {
+	showTicketsModal();
+	return false;
+});
+
+/*
+ * $(window).on('hashchange', function() { showTicketsModal(); });
+ */
 
