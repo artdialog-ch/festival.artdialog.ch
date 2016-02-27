26121016f4babb0131d68dc6e96bdb7e177174f0
Roman Mukhin <ramses_ru@hotmail.com>
2016-02-27 16:19:26 +0100

    added buy button for laila

---
+++ www/btn_prod.php
@@ -0,0 +1,14 @@
+<?php
+$btns_de = array(
+  'CERF1905......' => 'XXX',
+);
+
+$btns_fr = array(
+  'CERF1905' => 'XXX',
+);
+
+$btn_data = array(
+  'de' => $btns_de,
+  'fr' => $btns_fr,
+);
+?>
--- www/btn_sandbox.php
+++ www/btn_sandbox.php
@@ -1,10 +1,12 @@
 <?php
 $btns_de = array(
 	'CERF1905' => 'DL6ZSQKL6952G',
+	'LFGB2605' => '4K9R97BJDSZHC',
 );
 
 $btns_fr = array(
 	'CERF1905' => 'WS2GR8EN668N2',
+	'LFGB2605' => 'WYS7SDSXQUZFA',
 );
 
 $btn_data = array(
