7d4b8ab82c511fa590fd5c7e425db3828a0c04b2
dimitri-schweiz <d_vasylyev@hotmail.com>
2016-02-28 11:04:51 +0100

    btn paypay

--- www/btn_prod.php
+++ www/btn_prod.php
@@ -1,14 +1,34 @@
 <?php
 $btns_de = array(
-  'CERF1905......' => 'XXX',
+	'CERF1905' => 'FYGEA95JTV5AN',
+	'LFGB2605' => 'QCTHSJ7L44Z6Q',
+	'AVMO2705' => 'VEKD36X4Z2T3A',
+	'AVNE2805' => 'EAZVXARE6TDF2',
+	'ANBA0906' => 'A6XSX4KWZXCE8',
+	'KIBI0206' => 'DQJ4BACTV2AJ8',
+	'KIMO0306' => 'VQB6S3TU2QMYQ',	
+	'BABI2105' => '44TNYGCRNXSRL',
+	'RANE2205' => 'UTX7N7FYR89DW',
+	'PUBI0506' => 'YB7JXQGRVU862',
 );
 
 $btns_fr = array(
-  'CERF1905' => 'XXX',
+	'CERF1905' => 'GDTPQHVSGB45N',
+	'LFGB2605' => 'GY3TJY35VRMXY',
+	'AVMO2705' => 'CJTSKDSD3BPH2',
+	'AVNE2805' => 'V2Q62SXSLZ45A',
+	'ANBA0906' => '4M24WNEKFBR5C',
+	'KIBI0206' => 'CWSZZTLARUQDY',
+	'KIMO0306' => '8EFMH25PZVJTE',
+	'BABI2105' => 'ME7P8TA5YA67A',
+	'RANE2205' => 'G6R4YRHNKCS5C',
+	'PUBI0506' => '8P655RDN36YV2',
 );
 
 $btn_data = array(
-  'de' => $btns_de,
-  'fr' => $btns_fr,
+	'de' => $btns_de,
+	'fr' => $btns_fr,
 );
 ?>
+
+
