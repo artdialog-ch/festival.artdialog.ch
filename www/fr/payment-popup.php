440d0b78f5ab3389160d645d285bdbc1c9bcf332
dimitri-schweiz <d_vasylyev@hotmail.com>
2016-02-28 17:15:35 +0100

    link tickets

--- www/fr/payment-popup.php
+++ www/fr/payment-popup.php
@@ -17,7 +17,7 @@ require '../ppbuttons.php';
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title">Bestellung - <?php echo $event_title ?></h4>
+				<h4 class="modal-title">Commande - <?php echo $event_title ?></h4>
 			</div>
 			<div class="modal-body">
 				<p class="text-info">
