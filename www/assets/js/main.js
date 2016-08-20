8f0fc51f0ef1fff8d61a78573fde68dc9b0faf74
Roman Mukhin <ramses_ru@hotmail.com>
2016-08-20 22:17:59 +0200

    added partital html for nav-bar and jsscript to handle active state css-class

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -4,7 +4,7 @@ function showTicketsModal() {
 
 function getLang() {
 	var q = $('html[lang]');
-	return q.length ? q[0].lang : 'de'; 
+	return q.length ? q[0].lang : 'de';
 }
 
 var ticketATagSelector = 'a[href="#tickets"]';
@@ -30,13 +30,13 @@ function doLoadTicketsModal(eventId)
 	var loadurl = "payment-popup.php?eventid=" + encodeURIComponent(eventId) + "&lang=" + encodeURIComponent(lang);
 	$("#TicketsModal").load(loadurl + "#" + eventId, function( response, status, xhr ) {
 	  if ( status != "error" ) {
-	  	showTicketsModal();	  	
+	  	showTicketsModal();
 	  }
 	  else
 	  {
 	  	$("#ticket-cancel").removeClass("hidden");
 	  }
-	});	
+	});
 }
 
 $(document).ready(function() {
@@ -46,17 +46,35 @@ $(document).ready(function() {
 	if (hStr.indexOf('tickets') >= 0) {
 		loadTicketsModal(getEventId());
 	}
-	
-	var qStr = window.location.search.substring(1)  
+
+	var qStr = window.location.search.substring(1)
   if (qStr.indexOf('paypal=success') >= 0) {
     $("#ticket-success").removeClass("hidden");
   }
   else if (qStr.indexOf('paypal=cancel') >= 0) {
-    $("#ticket-cancel").removeClass("hidden");        	
+    $("#ticket-cancel").removeClass("hidden");
   }});
 
 $(ticketATagSelector).click(function() {
 	var eventId = getEventId(this);
 	loadTicketsModal(eventId);
 	return false;
+});
+
+$( document ).ready(function() {
+		var partNavBar = $('#partial-nav-bar')
+		if (partNavBar.length > 0)
+		{
+				partNavBar.load("partial/nav-bar.html", function() {
+
+				$(this).children(':first').unwrap();
+
+				var href = document.location.href;
+				var filename = href.substr(href.lastIndexOf('/') + 1);
+
+				if (/detail_.+/.test(filename)) filename = "programm.html"
+
+				$("a[href='" + filename + "']").parent().addClass("active")
+			});
+		}
 });
